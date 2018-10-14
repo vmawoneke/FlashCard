a2c4f87e340b18ab9d94ac8a55bc2b66c589fed1
Vimbai Mawoneke <kingjinspired@vimbais-macbook-pro.local>
2018-10-13 23:22:38 -0400

    Initial Commit

---
+++ FlashCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashCard
+//
+//  Created by Vimbai Mawoneke on 10/13/18.
+//  Copyright © 2018 Vimbai Mawoneke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
